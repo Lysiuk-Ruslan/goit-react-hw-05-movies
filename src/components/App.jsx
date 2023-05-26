7537fd99a4872250fae33bdc6f66cadc54fed5ec
Lisiuk-Ruslan <114481433+Lysiuk-Ruslan@users.noreply.github.com>
2023-05-27 01:28:50 +0300

    Зробив маршрутизацію

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,22 @@
+import Home from 'pages/Home/Home';
+import MovieDetails from 'pages/Movies/MovieDetails';
+import Movies from 'pages/Movies/Movies';
+import { Route, Routes } from 'react-router-dom';
+import { SharedLayout } from './SharedLayout/SharedLayout';
+import { Reviews } from './Reviews/Reviews';
+import { Cast } from './Cast/Cast';
+
 export default function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="movies" element={<Movies />} />
+        <Route path="movies/:movieId" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+      </Route>
+    </Routes>
+  );
 }
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <h1>trending today</h1>;
+};
+
+export default Home;
